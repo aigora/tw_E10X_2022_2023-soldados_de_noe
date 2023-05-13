22852df3b77537b4aa4f382728cf96e239dcf1c5
rafaelbarretodv <rafael.barreto@alumnos.upm.es>
2023-05-13 10:34:00 +0100

    Update Menu.c

--- Menu.c
+++ Menu.c
@@ -1,10 +1,18 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef struct{
     int meses;
     int ano;
-}calendario; //Estructura donde almacenar los 2 datos de la fecha
+} calendario; //Estructura donde almacenar los 2 datos de la fecha
 
+
+typedef struct{
+        char tipo_energia[40];
+        char x[24][40];
+        } energia;
+        
+/*        
 typedef struct{
     calendario fecha;
     double hidraulica;
@@ -24,53 +32,76 @@ typedef struct{
     double cogeneracion;
     double residuosNo; //residuos no renovables
     double residuosRenovables; //residuos renovables
-    double generacion; //generación total
-}datos;
-
+    double generacion; //generacion total
+}energia;
+*/
 //FUNCIONES
-void menu(); //Función donde realizar el menu
-void leerfichero (datos []); //Función para leer el fichero y guardar la información en la estructura datos
+void menu(energia E[]); //Funcion donde realizar el menu
+void leerfichero(energia E[]); //Funcion para leer el fichero y guardar la información en la estructura datos
 void tabla(); //Función para poner lo datos de una fecha en formato tabla/matriz
 void datoconcreto(); //Función para pedir un dato concreto de una fecha concreta
 void estadistica(); //Función que realiza opciones estadisticas
 
-
+#define N 18
 int main()
 {
-    menu();
+    
+    energia E[N]; //Vector estructura para almacenar los datos de las energias de cada fecha
+    menu(E);
     return 0;
 }
 
-void menu()
+void menu(energia E[])
 {
     int i;
-    datos energia[24]; //Vector estructura para almacenar los datos de las energias de cada fecha
-    leerfichero (energia);
-    printf("Bienvenid@ al menu. Elige la opción que desees: \n");
+    leerfichero(E);
+    printf("Bienvenido/a al menu. Elige la opcion que desees: \n");
 }
 
-void leerfichero (datos x[])
+void leerfichero (energia E[])
 {
     int i, j, z;
+    float valor=0.0;
     char linea[1000], titulo[30], cadnum[30];
     float numero;
-    FILE *fichero;
-    fichero = fopen("generacion_por_tecnologias_21_22.csv", "r"); //Abrimos el fichero en modo lectura
-    if (fichero == NULL)
-    {
+    FILE *fichero=NULL;
+    
+    //fichero=fopen("C:\\doc\\per\\pruebac\\prueba.txt","rt");
+    fichero=fopen(".\\generacion_por_tecnologias_21_22_puntos.csv","rt"); //Abrimos el fichero en modo lectura
+    if(fichero==NULL)
         printf("No se pudo abrir el archivo");
-    }
+    
     fgets(linea, 1000, fichero); // Saltar las cuatro primeras líneas (títulos)
+    printf("\n%s\n", linea);
+    
     fgets(linea, 1000, fichero);
     fgets(linea, 1000, fichero);
     fgets(linea, 1000, fichero);
-
-    fgets(linea, 1000, fichero); //Recoge la quinta linea (la de las fechas)
-    //printf("\n%s\n", linea);
-    char *separar = strtok(linea, ","); //Va escogiendo los valores/caracteres de la linea separados por comas
-    //printf("\n%s\n", separar);
-    sscanf(separar, "%s", &titulo);
+    fgets(linea, 1000, fichero);
+    
+    
+    for(i=0;i<18;i++)
+    {
+     fgets(linea, 1000, fichero); //Recoge la quinta linea (la de las fechas)
+     printf("\n%s\n", linea);
+     char *separar = strtok(linea,","); //Va escogiendo los valores/caracteres de la linea separados por comas
+     sscanf(separar,"%s",E[0].tipo_energia);
+     printf("%s\n",E[0].tipo_energia);
+     for(j=0;j<24;j++)
+     {
+      separar = strtok(NULL,",");
+      puts(separar);
+      sscanf(separar,E[i].x[j]);
+      printf("%s\n",E[i].x[j]);
+     }
+     getchar();
+    }
+    system("pause");
+    
+    getchar();
     //printf("El titulo es %s", titulo);
+}
+/* 
     i=0;
     while(separar != NULL ) {
         //printf( "%s\n", separar);
@@ -82,7 +113,8 @@ void leerfichero (datos x[])
 
 
     fgets(linea, 1000, fichero); //Recoge la sexta linea (hidraulica)
-    //printf("Esta es la cadena recogida: \n%s\n", linea);
+    printf("Esta es la cadena recogida: \n%s\n", linea);
+    getchar();
     i=-1;
     do{ //Bucle para cambiar las comas de los numeros por puntos
         i++;
@@ -124,3 +156,4 @@ void leerfichero (datos x[])
 
     fclose(fichero);
 }
+*/
